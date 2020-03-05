ae1547a0ced346bd45ad585a6498f7328661bb09
Vikram Ramanathan <vikramjb@gmail.com>
2020-03-06 05:12:52 +1100

    1. Initial Commit with basic model structure

---
+++ Models/Product.cs
@@ -0,0 +1,10 @@
+namespace ProdHiFiApi.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public string Description { get; set; }
+        public string Model { get; set; }
+        public string Brand { get; set; }
+    }
+}
